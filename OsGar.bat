af0f2e9117ff34c389ce9ccec8a240e19b26e185
gubrus50 <gabrys.234@wp.pl>
2015-08-31 20:26:44 +0100

    Server

--- OsGar.bat
+++ OsGar.bat
@@ -1,26 +1,144 @@
 #OsGar.bat#
+
+############
+SYSTEM(s)CM
+############
+
 @echo off
-color f
+color 4
 
-goto password
-
-:password
-cls
-set /p PASSWORD1= PORT:
-
-for /f "Delims"= %%a (port.txt) do (
-
-set TEXT=%%a
-)
-
-if %PASSWORD1% %TEXT% goto start
-echo This is not correct PORT
-pause
-goto :password
-
-:start
+:system
 cls
 color 0a
 echo -made by McRaZickPL
-pause
+echo 5
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo 4
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo 3
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo 2
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo 1
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo 0
+ping localhost -n 2 >nul
+cls
+goto system2
+
+:system2
+cls
+color f
+echo Welcome to OsGar, Made by McRaZickPL                         command -help
+echo Running on port 45455
+echo Server to Agar.io Free for all
+echo.
+set /p PROGRAM= CM:
+goto %PROGRAM%
+
+:help
+echo ==========================================================================
+echo -help    Opening the list that might help you with some staff
+echo -home    Back to menu) 
+echo -OsGar   Connect to the Agar.io
+echo -reload  Reload any changes
+echo -clear   Clear screan
+echo ==========================================================================
+set /p PROGRAM= CM:
+goto %PROGRAM%
+
+#############
+COMMAND LIST
+#############
+
+:home
+cls
+goto system2
+
+:OsGar
+cls
+echo -made by McRaZickPL
+echo Connecting.
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting..
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting...
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting.
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting..
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting...
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting.
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting..
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo Connecting...
+ping localhost -n 2 >nul
+cls
+echo -made by McRaZickPL
+echo ...
+ping localhost -n 5 >nul
+cls
+color 6
+cls
+echo Cannot connect to the localhost
+echo 5
+ping localhost -n 2 >nul
+cls
+echo Cannot connect to the localhost
+echo 4
+ping localhost -n 2 >nul
+cls
+echo Cannot connect to the localhost
+echo 3
+ping localhost -n 2 >nul
+cls
+echo Cannot connect to the localhost
+echo 2
+ping localhost -n 2 >nul
+cls
+echo Cannot connect to the localhost
+echo 1
+ping localhost -n 2 >nul
+cls
+echo Cannot connect to the localhost
+echo 0
+ping localhost -n 2 >nul
+color f
+goto system2
+
+
+:reload
+
+:clear
+cls
+goto system2
 
